0ac99fbee09dc357a1c9f105f6493e8d66ae2c37
Denis Libin <dnslibin4@gmail.com>
2024-06-30 16:58:00 +0300

    Finish about-time

---
+++ about-time.js
@@ -0,0 +1,19 @@
+import ms from 'ms'
+import fs from 'fs'
+
+
+// console.log(ms(60000))           
+console.log(ms(60000, { long: true }))
+
+formatTime()
+
+function formatTime() {
+    fs.readFile('time-stamps.txt', 'utf8', (err, content) => {
+        if (err) return console.log('Cannot read file', err)
+        const stamps = content.split('\r\n')
+        const now = Date.now()
+        console.log('now', now)
+        const formattedTime = stamps.map(stamp => ms(now - (stamp * 1000), { long: true }))
+        console.log(formattedTime.join(' '))
+    })
+}
