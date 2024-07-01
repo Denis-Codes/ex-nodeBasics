308b7fc2cf80c927a3f7aad481327adf27d3fac4
Denis Libin <dnslibin4@gmail.com>
2024-07-01 14:01:28 +0300

    Clean up and minor adjustments

--- js/about-time.js
+++ js/about-time.js
@@ -2,7 +2,7 @@ import ms from 'ms'
 import fs from 'fs'
 
 
-// console.log(ms(60000))           
+     
 console.log(ms(60000, { long: true }))
 
 formatTime()
@@ -12,8 +12,7 @@ function formatTime() {
         if (err) return console.log('Cannot read file', err)
         const stamps = content.split('\r\n')
         const now = Date.now()
-        console.log('now', now)
         const formattedTime = stamps.map(stamp => ms(now - (stamp * 1000), { long: true }))
-        console.log(formattedTime.join(' '))
+        console.log(formattedTime.join(('\r\n')))
     })
 }
--- js/draw-to-file.js
+++ js/draw-to-file.js
@@ -15,17 +15,17 @@ function writeToFile(str) {
 
 drawSquareToFile()
 function drawSquareToFile() {
- const str = getSquare(utilService.getRandomIntInclusive(3, 20))
- writeToFile(str)
- .then(() => {
- setTimeout(drawSquareToFile, 200)
- })
+    const str = getSquare(utilService.getRandomIntInclusive(3, 20))
+    writeToFile(str)
+        .then(() => {
+            setTimeout(drawSquareToFile, 200)
+        })
 }
 function getSquare(size) {
- var str = '*'.repeat(size) + '\n'
- for (let i = 0; i < size; i++) {
- str += '*' + ' '.repeat(size - 2) + '*\n'
- }
- str += '*'.repeat(size) + '\n'
- return str
+    var str = '*'.repeat(size) + '\n'
+    for (let i = 0; i < size; i++) {
+        str += '*' + ' '.repeat(size - 2) + '*\n'
+    }
+    str += '*'.repeat(size) + '\n'
+    return str
 }
